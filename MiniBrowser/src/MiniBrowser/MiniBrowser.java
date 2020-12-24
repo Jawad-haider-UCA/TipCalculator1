3f731eec31e356baacd3fa255563a3c5c3a0cea4
Jawad-haider-UCA <supine.me@gmail.com>
2020-12-24 12:41:52 -0600

    checking error

--- MiniBrowser/src/MiniBrowser/MiniBrowser.java
+++ MiniBrowser/src/MiniBrowser/MiniBrowser.java
@@ -6,16 +6,20 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 public class MiniBrowser extends Application {
 
-    public void start(Stage stage) throws Exception {
-        Parent root;
-        root = FXMLLoader.load(getClass().getResource("MiniBrowser.fxml"));
-        stage.setTitle("MiniBrowser");
-        stage.setScene(new Scene(root));
-        stage.show();
-
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("MiniBrowser.fxml"));
+        primaryStage.setTitle("MiniBrowser");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 
-    public static void main(String[] args) { launch(args); }
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
 }
--- MiniBrowser/src/MiniBrowser/MiniBrowserController.java
+++ MiniBrowser/src/MiniBrowser/MiniBrowserController.java
@@ -1,16 +1,18 @@
 package MiniBrowser;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
+
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
-public class MiniBrowserController implements Initializable {
+public class MiniBrowserController  implements Initializable {
 
     String https = "https://";
     String addressLink;
@@ -18,38 +20,51 @@ public class MiniBrowserController implements Initializable {
     ArrayList<String> history = new ArrayList<String>();
     int currPage = 0;
 
-    @FXML TextField addressBar;
-    @FXML WebView web;
-    @FXML Button prevButton;
-    @FXML Button nextButton;
-    public void browse(){
-        addressLink = addressBar.getText();
+    @FXML private Button backButton;
+
+    @FXML private Button nextButton;
+
+    @FXML private TextField goTextField;
+
+    @FXML private Button goButton;
+
+    @FXML private WebView web;
+
+    @FXML
+    void backButtonPressed(ActionEvent event) {
+        engine.load(history.get(--currPage));
+        nextButton.setDisable(false);
+        if(currPage == 0) backButton.setDisable(true);
+        goTextField.setText(history.get(currPage));
+
+    }
+
+    @FXML
+    void nextButtonPressed(ActionEvent event) {
+        engine.load(history.get(++currPage));
+        backButton.setDisable(false);
+        if(currPage == history.size() - 1) nextButton.setDisable(true);
+        goTextField.setText(history.get(currPage));
+
+    }
+
+    @FXML
+    void search(ActionEvent event) {
+        addressLink = goTextField.getText();
         engine.load(https+addressLink);
         history.add(https + addressLink);
         currPage = history.size() - 1;
-        prevButton.setDisable(false);
+        backButton.setDisable(false);
         nextButton.setDisable(true);
-    }
 
+
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         engine = web.getEngine();
         engine.load(https+"www.google.com");
         history.add(https+"www.google.com");
-    }
 
-    public void prevButtonClicked() {
-        engine.load(history.get(--currPage));
-        nextButton.setDisable(false);
-        if(currPage == 0) prevButton.setDisable(true);
-        addressBar.setText(history.get(currPage));
-    }
-
-    public void nextButtonClicked() {
-        engine.load(history.get(++currPage));
-        prevButton.setDisable(false);
-        if(currPage == history.size() - 1) nextButton.setDisable(true);
-        addressBar.setText(history.get(currPage));
     }
 }
